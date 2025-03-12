e27fa329145a49a19f594edc9c379be6ef92f8a2
TheTutel <timofey.achieve@gmail.com>
2025-03-12 18:29:48 +0200

    Все работает

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-tasks = []
+tasks = ['1. Interstellar','2. Breaking Bad','3. Go and see']
 
 while 1 == 1:
     len_true = len(tasks) + 1
@@ -15,27 +15,35 @@ while 1 == 1:
 
     def task_done(index):
         if 1<=index<=len(tasks):
-            tasks[index-1]=" (done) " + tasks[index-1]
+            tasks[index-1]=" (посмотрел) " + tasks[index-1]
 
     def task_undone(index):
         if 1<=index<=len(tasks):
-            if tasks[index-1][:8] == " (done) ":
-                tasks[index-1] = tasks[index-1][8:]
+            if tasks[index-1][:13] == " (посмотрел) ":
+                tasks[index-1] = tasks[index-1][13:]
+            else:
+                print("\nТы не отмечал его как посмотренный")
 
-    choice = input("\nВыберите действие\n1. Отметить задачу как выполненную\n2. Отметить задачу как невыполненную\n3. Добавить задачу\n4. Удалить задачу\n5. Выход\n\n")
+    choice = input("\nВыберите действие\n1. Отметить фильм как просмотренный\n2. Отметить фильм как непросмотренный\n3. Добавить фильм\n4. Удалить фильм\n5. Выход\n\n")
 
     if choice == "1":
-        index_select = int(input("Выберите задачу: "))
+        index_select = int(input("Выберите фильм: "))
+        if index_select != int:
+            "bruh"
         task_done(index_select)
 
     if choice == "2":
-        index_select = int(input("Выберите задачу: "))
+        index_select = int(input("Выберите фильм: "))
+        if index_select != int:
+            "bruh"
         task_undone(index_select)
 
-    if choice == "3": print("New task: "); task_appends()
+    if choice == "3": print("Новый фильм: "); task_appends()
 
     if choice == "4":
-        index_select = int(input("Выберите задачу (номер): "))
+        index_select = int(input("Выберите фильм: "))
+        if index_select != int:
+            "bruh"
         task_pop(index_select)
 
     if choice == "5":
